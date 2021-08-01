f2157c2ebb07ecf0e2df9490aa8a9a4cab332669
George Diepiriye <georgediepiriye4u@gmail.com>
2021-08-01 23:19:52 +0100

    created model and migration for order,order item,shipping and transaction

---
+++ database/migrations/2021_08_01_215910_create_orders_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->integer('subtotal');
+            $table->integer('discount')->default(0);
+            $table->integer('tax');
+            $table->integer('total');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('mobile');
+            $table->string('email');
+            $table->string('line1');
+            $table->string('line2')->nullable();
+            $table->string('city');
+            $table->string('province');
+            $table->string('country');
+            $table->string('zipcode');
+            $table->enum('status',['ordered','delivered','canceled'])->default('ordered');
+            $table->boolean('is_shipping_different')->default(false);
+            
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ database/migrations/2021_08_01_220019_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('order_id')->references('id')->on('orders');
+            $table->enum('mode',['cod','card','paypal']);
+            $table->enum('status',['pending','approved','desclined','refunded'])->default('pending');
+            $table->timestamps();
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
