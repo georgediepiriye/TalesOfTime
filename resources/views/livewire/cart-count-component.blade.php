20bdedd8ea180c68148c0e4f8b02c90bdc0308b4
George Diepiriye <georgediepiriye4u@gmail.com>
2021-08-02 14:18:24 +0100

    update

--- resources/views/livewire/cart-count-component.blade.php
+++ resources/views/livewire/cart-count-component.blade.php
@@ -1,7 +1,7 @@
 <div class="attr-nav">
     <ul>
        
-        <li class="side-menu"><a href="{{ route('product.cart') }}">
+        <li><a href="{{ route('product.cart') }}">
          <span ><b style="font-size: 16px !important;">Cart</b></span>   
         <i class="fa fa-shopping-bag"></i>
             <span class="badge">@if (Cart::count()>0)
